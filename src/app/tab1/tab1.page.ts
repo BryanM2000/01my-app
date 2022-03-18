c1cfe02888c875cda18443bfb579a0ae95caeeb0
Bryan Merino <bryanmerinoh@hotmail.com>
2022-03-17 20:47:10 -0500

    Fin seccion 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
   constructor() {}
+
+
+  ngOnInit() {
+
+    let numero = 90;
+
+    // debugger siempre temporal, cuando ya no lo necesito borrarlo
+    //me ayuda a llegar directo a este punto en el navegador
+    //debugger; 
+
+    numero = 20;
+
+    numero = numero +30;
+
+    numero = 0;
+
+    console.log(numero);
+    
+  }
 
 }
